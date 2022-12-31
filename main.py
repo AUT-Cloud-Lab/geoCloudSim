c0b2f62a4220fbfeadc27243d2a5ad148da2ddf0
momtazpour <momtazpour@ceit.local>
2022-12-31 20:13:31 +0330

    Added pyCloudSim class

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # This is pyCloudSim, a cloud simulator written in Python.
 # It is developed and maintained by M. Momtazpour
 
+from PyCloudSim import PyCloudSim
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def print_hi():
+    print(f'Welcome to pyCloudSim')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
+    print_hi()
+    simTime = 1000
+    sim = PyCloudSim(simTime)
+    print(sim.get_time())
--- pyCloudSim.py
+++ pyCloudSim.py
@@ -1,6 +1,21 @@
 # This is pyCloudSim class, which is used to initialize, start and stop the simulator
 # @author Mahmoud Momtazpour
 
-class pyCloudSim:
-        pass
+class PyCloudSim(object):
+    simTime = 100
 
+    def __init__(self, time=None):
+        if time is not None:
+            if isinstance(time, int) and time > 0:
+                self.simTime = time
+            else:
+                print('The value should be a positive integer, default value of 100 will be used.')
+
+    def initialize(self, time):
+        if isinstance(time, int) and time > 0:
+            self.simTime = time
+        else:
+            print('The value should be a positive integer, default value of 100 will be used.')
+
+    def get_time(self):
+        return self.simTime
