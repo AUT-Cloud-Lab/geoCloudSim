f0a3be593652122bb5337c436e756a48445e5272
momtazpour <momtazpour@ceit.local>
2023-01-02 00:43:39 +0330

    Added VMAllocationPolicy Class

---
+++ VMAllocationPolicy.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class VMAllocationPolicy(ABC):
+    def __init__(self, host_list):
+        self.__host_list = host_list
+
+    @abstractmethod
+    def allocate_host_for_vm(self, vm):
+        pass
+
+    @abstractmethod
+    def allocate_host_for_vm(self, vm, host):
+        pass
+
+    @abstractmethod
+    def optimize_allocation(self, vm_list):
+        pass
+
+    @abstractmethod
+    def deallocate_host_for_vm(self, vm):
+        pass
+
+    @abstractmethod
+    def get_host(self, vm):
+        pass
+
+    def get_host_list(self):
+        return self.__host_list
+
+    def _set_host_list(self, host_list):
+        self.__host_list = host_list
